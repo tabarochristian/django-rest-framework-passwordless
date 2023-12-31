868cf82cf836d1372d6ad012d41df68b32e86acd
Tabaro Christian <tabarochristian@yahoo.com>
2024-01-01 05:42:51 +0800

    Update serializers.py

--- drfpasswordless/serializers.py
+++ drfpasswordless/serializers.py
@@ -43,10 +43,8 @@ class AbstractBaseAliasAuthenticationSerializer(serializers.Serializer):
 
             if api_settings.PASSWORDLESS_REGISTER_NEW_USERS is True:
                 # If new aliases should register new users.
-                try:
-                    user = User.objects.get(**{self.alias_type+'__iexact': alias})
-                except User.DoesNotExist:
-                    user = User.objects.create(**{self.alias_type: alias})
+                user, created = User.objects.get_or_create(**{self.alias_type: alias})
+                if created:
                     user.set_unusable_password()
                     user.save()
             else:
